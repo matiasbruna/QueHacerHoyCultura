56c94c57e5ca0df629ad31ac2d426515280a519a
Matias <matias.b.dev@gmail.com>
2023-11-13 02:44:33 -0300

    Agregar archivos de proyecto.

---
+++ QueHacerHoyCultura/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QueHacerHoyCultura.Entidades;
+
+namespace QueHacerHoyCultura
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Provincia> Provincias => Set<Provincia>();
+    }
+}
---
+++ QueHacerHoyCultura/Migrations/20231113053531_SegundaMigracion.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueHacerHoyCultura.Migrations
+{
+    /// <inheritdoc />
+    public partial class SegundaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_provincias",
+                table: "provincias");
+
+            migrationBuilder.RenameTable(
+                name: "provincias",
+                newName: "Provincias");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Provincias",
+                table: "Provincias",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Provincias",
+                table: "Provincias");
+
+            migrationBuilder.RenameTable(
+                name: "Provincias",
+                newName: "provincias");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_provincias",
+                table: "provincias",
+                column: "Id");
+        }
+    }
+}
